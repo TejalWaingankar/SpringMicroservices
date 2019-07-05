9da1479b1639a8eb99adc78083449578ec70b3f9
TejalWaingankar <tejal.waingankar@gmail.com>
2019-07-05 20:42:58 +0530

    Added fault tolerance support using Hystrix.

--- src/main/java/com/sync/limitsservice/controller/LimitsServiceController.java
+++ src/main/java/com/sync/limitsservice/controller/LimitsServiceController.java
@@ -6,6 +6,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sync.limitsservice.controller.config.AppConfiguration;
 import com.sync.limitsservice.controller.model.LimitConfiguration;
 
@@ -20,6 +21,16 @@ public class LimitsServiceController {
 		return new LimitConfiguration(config.getMinimum(), config.getMaximum());
 	}
 
+	@GetMapping("/wrong-limits")
+	@HystrixCommand(fallbackMethod="handleWrongLimitsService")
+	public LimitConfiguration getWrongLimitsServiceConfig() {
+		throw new RuntimeException("Wrong Limits Call");
+	}
+	
+	public LimitConfiguration handleWrongLimitsService() {
+		return new LimitConfiguration(1000, 2000);
+	}
+	
 	/*
 	 * Using @Value to read properties file values.
 		@Value("${limits.service.minimum}")
